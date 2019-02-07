75e4e0bb72d0a6f4371005d13d00c9354775039d
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2019-02-08 01:07:50 +0900

    refectoring

--- main.js
+++ main.js
@@ -1,75 +1,107 @@
-function isHide(url) {
-  return new Promise(function(resolve, reject){
-    chrome.storage.local.get([url], function(value){
-      const keys = Object.keys(value);
-      if (value[keys[0]]) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
+class SearchBase {
+  constructor() {
+    this.frameClassName = '';
+    this.trashBoxButtonIdPrefix = 'trashBox';
+    this.pickUpTrashButtonIdPrefix = 'pickUpTrash';
+    this.hiddenContentButtonId = 'hiddenContentButton';
+    this.trashUrlBackgroundColor = 'lightgrey';
+  }
+
+  isHide(url) {
+    return new Promise(function(resolve, reject){
+      chrome.storage.local.get([url], function(value){
+        const keys = Object.keys(value);
+        if (value[keys[0]]) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+    })
+  }
+
+  addPickUpTrashButton(element, index) {
+    // interface
+  }
+
+  declareTrashBoxButton(element, url, index) {
+    const id_name = this.trashBoxButtonIdPrefix + index;
+    $('#' + id_name).on('click', () => {
+      chrome.storage.local.set({[url] : true}, function(){
+      });
+      $(element).hide();
+      $(element).css('background-color', this.trashUrlBackgroundColor);
+      $('#' + id_name).hide();
+      this.addPickUpTrashButton(element, index);
     });
-  })
-}
+  }
 
-function addTrashBoxButton(element, index) {
-  // ゴミ箱ボタンを作成
-  let id_name = 'trashBox' + index;
-  let action_menu = $(element).find('.action-menu' + '.ab_ctl');
-  action_menu.after('<button id=' + id_name + ' type="button">Trash</button>');
+  addTrashBoxButton(element, index) {
+    // interface
+  }
 
-  // ゴミ箱ボタンの動作を定義
-  $('#' + id_name).on('click', function() {
-    let url = $(element).find('a').attr('href');
-    chrome.storage.local.set({[url] : true}, function(){
+  declarePickUpTrashButton(element, url, index) {
+    const id_name = this.pickUpTrashButtonIdPrefix + index;
+    $('#' + id_name).on('click', () => {
+      chrome.storage.local.remove(url, function() {
+      });
+      $(element).show();
+      $(element).css('background-color', '');
+      $('#' + id_name).hide();
+      this.addTrashBoxButton(element, index);
     });
-    $(element).hide();
-    $(element).css('background-color', 'lightgrey');
-    $('#' + id_name).hide();
-    addPickUpTrashButton(element, index);
-  });
-}
+  }
 
-function addPickUpTrashButton(element, index) {
-  let id_name = 'pickUpTrash' + index;
-  let action_menu = $(element).find('.action-menu' + '.ab_ctl');
-  action_menu.after('<button id=' + id_name + ' type="button">PickUp</button');
+  addHiddenContentButton() {
+    let elements = $(this.frameClassName);
+    $(elements[elements.length - 1]).after('<button id=' + this.hiddenContentButtonId + ' type="button">HideContents</button>');
+    this.declareHiddenContentButton();
+  }
 
-  $('#' + id_name).on('click', function() {
-    let url = $(element).find('a').attr('href');
-    chrome.storage.local.remove(url, function() {
+  declareHiddenContentButton() {
+    $('#' + this.hiddenContentButtonId).on('click', () => {
+      $(this.frameClassName + ':hidden').show();
     });
-    $(element).show();
-    $(element).css('background-color', '');
-    $('#' + id_name).hide();
-    addTrashBoxButton(element, index);
-  });
+  }
 }
 
-function addHiddenContentButton() {
-  // 非表示サイトを表示するボタンを作成
-  let elements = $('.g');
-  $(elements[elements.length - 1]).after('<button id="hiddenContentButton" type="button">HideContents</button>');
+class GoogleSearch extends SearchBase {
+  constructor() {
+    super();
+    this.frameClassName = '.g';
+  }
 
-  // 表示ボタンの動作を定義
-  $('#hiddenContentButton').on('click', function() {
-    $('.g:hidden').show();
-  })
+  addTrashBoxButton(element, index) {
+    const id_name = this.trashBoxButtonIdPrefix + index;
+    let action_menu = $(element).find('.action-menu' + '.ab_ctl');
+    action_menu.after('<button id=' + id_name + ' type="button">Trash</button>');
+    const url = $(element).find('a').attr('href');
+    super.declareTrashBoxButton(element, url, index);
+  }
+
+  addPickUpTrashButton(element, index) {
+    const id_name = this.pickUpTrashButtonIdPrefix + index;
+    let action_menu = $(element).find('.action-menu' + '.ab_ctl');
+    action_menu.after('<button id=' + id_name + ' type="button">PickUp</button');
+    const url = $(element).find('a').attr('href');
+    super.declarePickUpTrashButton(element, url, index);
+  }
 }
 
+let google_search = new GoogleSearch();
 let elements = $('.g');
 for(let i = 0; i < elements.length; i++) {
   let url = $(elements[i]).find('a').attr('href');
 
-  isHide(url).then(function(hide){
+  google_search.isHide(url).then(function(hide){
     if(hide) {
       $(elements[i]).hide();
       $(elements[i]).css('background-color', 'lightgrey');
-      addPickUpTrashButton(elements[i], i+1);
+      google_search.addPickUpTrashButton(elements[i], i+1);
     } else {
-      addTrashBoxButton(elements[i], i+1);
-
+      google_search.addTrashBoxButton(elements[i], i+1);
     }
   });
 }
 
-addHiddenContentButton();
+google_search.addHiddenContentButton();
