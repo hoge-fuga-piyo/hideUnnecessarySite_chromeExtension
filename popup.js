0533adaa28f00d662ca98a53e1bd794bfdc0feae
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2019-02-17 23:30:00 +0900

    ポップアップのURLクリック時に新規タブで開くよう修正

--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
 chrome.storage.local.get(null, function(items) {
   let all_keys= Object.keys(items);
   for(let key of all_keys) {
-    $('#urls').append(key + '<br>');
+    $('#urls').append('<div><a href="' + decodeURIComponent(key) + '">' + decodeURIComponent(key) + '</a></div>');
   }
+  $('#urls').on('click', (e) => {
+    chrome.tabs.create({url: $(e.target).attr('href')});
+  });
 });
